04dee2f0adad5f0ceafb8d18baaecddfbdf1210f
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-18 15:46:17 -0600

    quotes

---
+++ src/lib/quotes.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+import { pb } from './pocketbase';
+
+  let quotes = [];
+
+  onMount(async () => {
+    // get quotes in list format
+    const quotesList = await pb.collection('quotes').getList(1,50 {
+        sort: '',
+        expand: 'author',
+    })
+  });
+quotes = quotesList.items;
+</script>
