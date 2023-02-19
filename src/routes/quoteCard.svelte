7a2edebbc8a191f56a250f3f7f486811483dc435
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-18 20:10:48 -0600

    quoteCard

---
+++ src/routes/quoteCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<div class="quote-container">
+  <blockquote>
+    <slot name="quoteText">
+      All right. Well, take care of yourself, Han. I guess that's what you're
+      best at, ain't it? All right. Well, take care of yourself, Han. I guess
+      that's what you're best at, ain't it? You are a part of the Rebel Alliance
+      and a traitor! Take her away! You mean it controls your actions? She must
+      have hidden the plans in the escape pod. Send a detachment down to
+      retrieve them, and see to it personally, Commander. There'll be no one to
+      stop us this time! Ye-ha! Escape is not his plan. I must face him, alone.
+      I don't know what you're talking about. I am a member of the Imperial
+      Senate on a diplomatic mission to Alderaan-- But with the blast shield
+      down, I can't even see! How am I supposed to fight?
+    </slot>
+  </blockquote>
+  <cite>
+    <span class="quote-author"><slot name="quoteAuthor">Star Wars</slot></span>
+    in
+    <span class="quote-source">
+      <slot name="quoteAuthor">fillerama</slot>
+    </span>
+  </cite>
+</div>
+
+<style>
+</style>
