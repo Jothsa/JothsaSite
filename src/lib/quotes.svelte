ecaf61551937b034b09aeea92f1ff709220f44a0
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-18 18:16:25 -0600

    pocketbase

--- src/lib/quotes.svelte
+++ src/lib/quotes.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-import { pb } from './pocketbase';
+  import { pb } from './pocketbase';
 
   let quotes = [];
 
   onMount(async () => {
     // get quotes in list format
-    const quotesList = await pb.collection('quotes').getList(1,50 {
-        sort: '',
-        expand: 'author',
-    })
+    const quotesList = await pb.collection('quotes').getList(1, 50, {
+      sort: '',
+      expand: 'author,source',
+    });
   });
-quotes = quotesList.items;
+  quotes = quotesList.items;
 </script>
