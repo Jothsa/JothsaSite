216ca210c1c09ea9c67545517aba219e5f64f9e6
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-20 17:47:53 -0600

    grad shadow

--- src/lib/quoteCard.svelte
+++ src/lib/quoteCard.svelte
@@ -45,16 +45,50 @@
   </cite>
 </div>
 
-<style>
+<style lang="postcss">
   .quoteContainer {
+    --bg-color: #f5f5f5;
+    --grad: radial-gradient(circle at top left, #f00, #3020ff);
+    --transition: 400ms scale 120ms ease-in-out;
     border-radius: 0.5em;
     padding: 0.25em;
     position: relative;
+    transform-style: preserve-3d;
     border: double 4px transparent;
-    background-image: linear-gradient(white, white),
-      radial-gradient(circle at top left, #f00, #3020ff);
+    background-image: linear-gradient(var(--bg-color), var(--bg-color)),
+      var(--grad);
     background-origin: border-box;
     background-clip: padding-box, border-box;
-    background-color: #f9f9f9;
+    transition: var(--transition);
+
+    &::before {
+      scale: 0.9; /* This way the shadow isn't visible at first. */
+      transition: var(--transition);
+      --spread: -3px;
+      --grad-shadow: var(--grad);
+      --x-offset: 8px;
+      --y-offset: 6px;
+      --blur: 10px;
+      content: '';
+      position: absolute;
+      border-radius: inherit;
+      inset: var(--spread);
+      transform: translate3d(
+        var(--x-offset),
+        var(--y-offset),
+        -1px
+      ); /* (X, Y, Z) */
+      background: var(--grad-shadow);
+      filter: blur(var(--blur));
+    }
+
+    /* technically the card isn't focusable. Maybe i should change that? */
+    &:hover,
+    &:focus-within {
+      scale: 1.025;
+      &::before {
+        scale: 1;
+      }
+    }
   }
 </style>
