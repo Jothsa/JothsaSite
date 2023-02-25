1713eb2599ff87a12b49623ffbacbd0e32726912
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-25 09:59:09 -0600

    add anim button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
   import '../app.postcss';
   import { SvelteUIProvider } from '@svelteuidev/core';
+  import { Button } from '@svelteuidev/core';
+  import { onMount } from 'svelte';
   import Nav from '../lib/navigation.svelte';
+
+  function toggleAnim() {
+    document.body.classList.toggle('gradBGAnim');
+  }
+
+  onMount(async () => {
+    document.body.classList.toggle('gradBGAnim');
+  });
 </script>
 
 <SvelteUIProvider>
   <Nav />
   <slot />
+  <Button on:click={toggleAnim}>Animate?</Button>
 </SvelteUIProvider>
