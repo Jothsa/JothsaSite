8b9b0734ea5dc6c7ca310f94ac65c750e54d9f90
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-02-26 21:58:01 -0600

    mixins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,12 @@
 const jit = require('postcss-jit-props');
 const presetEnv = require('postcss-preset-env');
 const openProps = require('open-props');
+const mixins = require('postcss-mixins');
 
 const config = {
   plugins: [
     jit(openProps),
+    mixins(),
     presetEnv({
       stage: 2,
       features: {
