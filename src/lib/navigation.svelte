485179314e4ba6cf0ac09dcc0c1aa01388375e0b
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-03 21:34:17 -0600

    lots of stuff

--- src/lib/navigation.svelte
+++ src/lib/navigation.svelte
@@ -15,23 +15,53 @@
 
   nav {
     display: grid;
-    padding: 0.25rem 2rem 5rem;
+    padding: min(2rem, 20vh) min(2rem, 20vh) min(5rem, 35vw);
     background-color: oklch(94.91% 0 0);
     grid-auto-flow: column;
     -webkit-mask: var(--mask-edge-drip-bottom);
     place-items: center;
 
+    @media (width < 25rem) {
+      grid-auto-flow: row;
+    }
+
     & a {
+      position: relative;
       display: inline-block;
-      padding: 1rem 0.5rem;
+      padding-bottom: min(0.3ch, 24vmax);
       color: oklch(45.2% 0.31321437166460125 264.052020638055);
       font-size: 1.125rem;
       text-align: center;
       text-decoration: none;
 
+      &::after {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: max(2px, 0.5ch);
+        background: linear-gradient(
+          90deg,
+          var(--random-color-1),
+          var(--random-color-2)
+        );
+        content: '';
+        transform: scaleX(0);
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+      }
+
       /* TODO add focus styles */
-      :hover {
+
+      &:hover {
         cursor: pointer;
+
+        &::after {
+          @media (prefers-reduced-motion: no-preference) {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+          }
+        }
       }
     }
   }
