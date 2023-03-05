61e64aa7cd34593a831c20e652487c9c3b62fce8
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-04 21:17:51 -0600

    server load wip

--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Quotes from '../../lib/quotes.svelte';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
 </script>
 
 <header>
   <h1>Quotes</h1>
 </header>
 
-<Quotes />
+<Quotes data.quotesList />
