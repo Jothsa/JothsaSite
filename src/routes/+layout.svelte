6543f8cc4deff803486457bc4abebb22ae60791e
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-04 21:31:51 -0600

    fix import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import { SvelteUIProvider } from '@svelteuidev/core';
   import { Button } from '@svelteuidev/core';
   import { onMount } from 'svelte';
-  import Nav from '../lib/Navigation.svelte';
+  import Nav from '$lib/Navigation.svelte';
   import Controls from '$lib/Controls.svelte';
   import 'agnostic-svelte/css/common.min.css';
 
