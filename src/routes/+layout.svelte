285b73d2c301f5f7424e4ee323c3f537c5693ffa
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-05 11:05:02 -0600

    removed agnostic, add layers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
   import { onMount } from 'svelte';
   import Nav from '$lib/Navigation.svelte';
   import Controls from '$lib/Controls.svelte';
-  import 'agnostic-svelte/css/common.min.css';
 
   function toggleAnim() {
     document.body.classList.toggle('anim');
