4e4a56191c72872fa7970263a9f049710e91ddf3
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-07 14:50:48 -0600

    cleanup

--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-  console.log('start');
   import Quotes from '$lib/quotes.svelte';
-  console.log('1');
   import type { PageData } from './$types';
-  console.log('2');
 
   export let data: PageData;
   $: quotesList = data.quotesList;
