e371c5c7d65591a8c47c358de3b153d664292253
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-07 15:10:14 -0600

    commented unused stuff

--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import Quotes from '$lib/quotes.svelte';
-  import type { PageData } from './$types';
+  // import type { PageData } from './$types';
 
-  export let data: PageData;
-  $: quotesList = data.quotesList;
+  // export let data: PageData;
+  // $: quotesList = data.quotesList;
 </script>
 
-<Quotes {quotesList} />
+<Quotes />
