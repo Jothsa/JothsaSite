66786ac35db7439b6b4db41095bb3a8bf68db94c
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-07 11:38:29 -0600

    create

---
+++ src/lib/PageTransition.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { fly, blur } from 'svelte/transition';
+  import { page } from '$app/stores';
+  import { cubicIn, cubicOut } from 'svelte/easing';
+  export let pathname: string;
+  export let duration = 300;
+  let blurDuration = duration / 3;
+  let delay = duration + 100;
+  export let y = 10;
+
+  const transitionInFly = {
+    easing: cubicOut,
+    y,
+    duration: duration * 0.5,
+    delay,
+  };
+  const transitionInBlur = {
+    easing: cubicOut,
+    y,
+    duration: blurDuration,
+    delay,
+  };
+  const transitionOutFly = { easing: cubicIn, y: -y, duration };
+</script>
+
+{#key pathname}
+  <div in:fly={transitionInFly} out:fly={transitionOutFly}>
+    <slot />
+  </div>
+{/key}
