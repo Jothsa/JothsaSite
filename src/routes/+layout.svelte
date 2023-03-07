3fcd71e27517ccee65f895430341a20ea884f520
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-07 10:54:49 -0600

    page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
-<script>
+<script lang="ts">
   import '../app.postcss';
   import { SvelteUIProvider } from '@svelteuidev/core';
   import { Button } from '@svelteuidev/core';
   import { onMount } from 'svelte';
   import Nav from '$lib/Navigation.svelte';
   import Controls from '$lib/Controls.svelte';
+  import PageTransition from '$lib/PageTransition.svelte';
+  import type { LayoutData } from './$types';
+  export let data: LayoutData;
 
   function toggleAnim() {
     document.body.classList.toggle('anim');
@@ -18,6 +21,8 @@
 <SvelteUIProvider>
   <Nav />
   <!-- <Controls /> -->
-  <slot />
-  <Button on:click={toggleAnim}>Animate?</Button>
+  <PageTransition pathname={data.pathname}>
+    <slot />
+    <Button on:click={toggleAnim}>Animate?</Button>
+  </PageTransition>
 </SvelteUIProvider>
