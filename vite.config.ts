5cc5ed9ba66824c84f10d72abca961864f957b1b
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-06 20:56:32 -0600

    fontinator

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import stylelint from 'vite-plugin-stylelint';
+import { FontaineTransform } from 'fontaine';
+
+const fontaineOptions = {
+  fallbacks: [
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Helvetica Neue',
+    'Arial',
+    'Noto Sans',
+    'Impact',
+  ],
+  // You may need to resolve assets like `/fonts/Roboto.woff2` to a particular directory
+  //resolvePath: (id) => 'file:///path/to/public/dir' + id,
+  // overrideName: (originalName) => `${name} override`
+  // sourcemap: false
+};
 
 export default defineConfig({
-  plugins: [sveltekit(), stylelint()],
+  plugins: [sveltekit(), stylelint(), FontaineTransform.vite(fontaineOptions)],
 });
