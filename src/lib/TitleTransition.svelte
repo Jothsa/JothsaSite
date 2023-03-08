b8bb48148e6d3a04bc1051c8688acdfe5a6725bb
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-07 19:49:07 -0600

    create

---
+++ src/lib/TitleTransition.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { blur } from 'svelte/transition';
+  import { cubicIn, cubicOut } from 'svelte/easing';
+  export let pathname: string;
+  export let duration = 300;
+  let delay = duration + 100;
+  export let y = 10;
+
+  const transitionInBlur = {
+    easing: cubicOut,
+    y,
+    duration: duration,
+    delay,
+  };
+  const transitionOutBlur = { easing: cubicIn, y: -y, duration };
+</script>
+
+{#key pathname}
+  <div in:blur={transitionInBlur} out:blur={transitionOutBlur}>
+    <slot />
+  </div>
+{/key}
