bb40cd6c946fc7ffa7724716403335054fe186c5
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-03-09 21:39:43 -0600

    removed svelte preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import importAssets from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
