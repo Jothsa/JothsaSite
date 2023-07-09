d056df99d8d94c960f66d00b0a44562fed53ba7d
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-08 21:55:02 -0500

    create

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+- [ ] add backups for vars?
---
+++ src/lib/components/Blurb.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let title: string;
+  export let theme: 'happy' | 'upset' | '' = '';
+
+  // TODO finish
+</script>
+
+<!-- TODO fix markup -->
+<div class="blurb-container {theme}">
+  <div class="blurb">
+    <h1>{title}</h1>
+    <p><slot /></p>
+  </div>
+</div>
+
+<style>
+  .blurb-container {
+    container: blurb / inline-size;
+  }
+
+  .blurb {
+    display: grid;
+
+    @container (inline-size > 30ch) {
+      /* TD */
+    }
+  }
+</style>
