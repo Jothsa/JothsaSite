467d9aad218bc9f2b9fc70177889907a93de1015
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-14 10:32:24 -0500

    cleanup

--- src/lib/components/NavNew.svelte
+++ src/lib/components/NavNew.svelte
@@ -132,12 +132,6 @@
         fill: var(--link-grad-color-1);
       }
 
-      @supports not selector(:focus-visible) {
-        &:focus {
-          outline: solid var(--nav-link-color) 2px;
-        }
-      }
-
       &:focus-visible {
         outline: solid var(--nav-link-color) 2px;
       }
@@ -192,95 +186,6 @@
         text-align: center;
 
         /* place-self: center; */
-
-        & span {
-          display: inline-flex;
-          flex-wrap: nowrap;
-          gap: var(--space-2xs);
-        }
-
-        & n {
-          --_nav-transition-time: calc(var(--nav-transition-time) * 1ms);
-          position: relative;
-          display: inline-block;
-          padding-block-end: clamp(2px, 0.1em, 0.2em);
-          -webkit-background-clip: text;
-          background-image: linear-gradient(
-            90deg,
-            var(--link-grad-color-1),
-            var(--link-grad-color-2) 50%,
-            var(--nav-link-color) 50%
-          );
-          background-position: -100%;
-          background-size: 200% 100%;
-          color: var(--nav-link-color);
-          font-family: var(--font), system-ui, sans-serif;
-          text-decoration: none;
-          -webkit-text-fill-color: transparent;
-
-          @media (prefers-reduced-motion: no-preference) {
-            /* stylelint-disable-next-line plugin/no-low-performance-animation-properties */
-            transition: background-position var(--_nav-transition-time)
-              ease-in-out;
-          }
-
-          /* should keep underline when sub item selected and or add another indicator to button? */
-          &[aria-current='page'],
-          [aria-current='page'] {
-            @media (prefers-contrast: no-preference) and (prefers-reduced-motion: no-preference) {
-              background-position: 0;
-            }
-            font-weight: 600;
-            transition: none;
-          }
-
-          &::after {
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: var(--underline-height);
-            background: linear-gradient(
-              90deg,
-              var(--link-grad-color-1),
-              var(--link-grad-color-2)
-            );
-            content: '';
-
-            @media (prefers-reduced-motion: no-preference) {
-              transform: scaleX(0);
-              transform-origin: bottom left;
-              transition: transform var(--_nav-transition-time) ease-out;
-            }
-
-            @media (prefers-reduced-motion) {
-              opacity: 0;
-              transition: opacity 0.3s ease-in-out;
-            }
-          }
-
-          /* TODO add focus styles */
-
-          &:hover,
-          &:focus {
-            @media (prefers-reduced-motion: no-preference) {
-              background-position: 0;
-            }
-            cursor: pointer;
-            outline: none;
-
-            &::after {
-              @media (prefers-reduced-motion: no-preference) {
-                transform: scaleX(1);
-                transform-origin: bottom left;
-              }
-
-              @media (prefers-reduced-motion) {
-                opacity: 1;
-              }
-            }
-          }
-        }
       }
     }
 
@@ -322,72 +227,11 @@
           grid-column: initial;
         }
 
-        & > li:has(ul) {
-          /* doesn't do anything-- should be contents */
-
-          /* display: content; */
-        }
-
         & > span {
           grid-column: 2 / 3;
 
           @container nav (inline-size > 30ch) {
             grid-column: initial;
-          }
-        }
-
-        & ul {
-          position: relative;
-          overflow: hidden;
-          min-inline-size: max-content;
-          height: 0;
-          border: solid oklch(87% 0 0) 2px;
-          border-radius: 4px;
-          background: var(--bg-color);
-          gap: clamp(2vmin, 0.2rem, 2vmax);
-          grid-column: 1 / 4;
-          hyphens: none;
-          opacity: 0;
-          overflow-wrap: normal;
-
-          /* TODO fix transform stacking (is behind cards on homepage) */
-          transform: rotateX(90deg), translate3d(0, 0, 5px);
-          transform-origin: top center;
-
-          /* 120ms is for hover intent */
-          transition: transform 280ms 120ms ease-in-out,
-            opacity 300ms 120ms ease-in-out;
-          visibility: hidden;
-
-          @container nav (inline-size > 30ch) {
-            position: absolute;
-            top: calc(100% - 0.25rem);
-            left: 0;
-            grid-column: initial;
-          }
-
-          @media (prefers-reduced-motion: no-preference) {
-            & a {
-              opacity: 0;
-              transform: translate(-100%);
-              transition: transform 240ms
-                  calc(var(--link-stagger) * 50ms + 50ms) ease-in-out,
-                opacity 240ms calc(var(--link-stagger) * 50ms + 100ms)
-                  ease-in-out;
-            }
-          }
-
-          &.expanded {
-            height: auto;
-            opacity: 1;
-            visibility: visible;
-
-            @media (prefers-reduced-motion: no-preference) {
-              & a {
-                opacity: 1;
-                transform: translate(0);
-              }
-            }
           }
         }
       }
