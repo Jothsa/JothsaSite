f7496e3e4f71cfe9eeb60810fa3147d1270ba3d0
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-16 16:46:45 -0500

    add categories

--- src/routes/blog/categories/+page.svelte
+++ src/routes/blog/categories/+page.svelte
@@ -1,3 +1,10 @@
 <script lang="ts">
   // TODO display all categories use type array thing to generate
+  import { CategoriesList } from '$utils/types';
 </script>
+
+<ul class="categories">
+  {#each CategoriesList as cat}
+    <li class="category"><a href={`/blog/categories/${cat}`}>{cat}</a></li>
+  {/each}
+</ul>
