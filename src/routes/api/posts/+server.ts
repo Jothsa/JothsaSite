28228c81d04cf20f459d17a70bdf1522bae04948
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-16 17:20:40 -0500

    cleanup

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -8,30 +8,21 @@ import type { Categories } from '$utils/types';
 
 export const GET = (async ({ request }) => {
   let posts: Post[] = await getPosts();
-  // return json(posts);
-  // log all headers
-  console.log(`headers: `, ...request.headers);
-  // should be a comma separated list or single value
+  // post-categories header should be a comma separated list or single value (or null)
   let categories: Categories[];
   const headerCatsRaw = request.headers.get('post-categories');
   let headerCats: string[] = [];
-  console.log(`headerCatsRaw: ${headerCatsRaw}`);
   if (headerCatsRaw) {
     if (headerCatsRaw && headerCatsRaw.includes(',')) {
       headerCats = headerCatsRaw?.split(',');
-      console.log(headerCats);
     } else if (headerCatsRaw) {
       headerCats = [headerCatsRaw];
-      console.log(headerCats);
     }
     if (isCategories(headerCats)) {
       categories = headerCats;
       posts = await getPostsByCategory(categories);
     }
   }
-  // else {
-  //   posts = await getPosts();
-  // }
 
   // create a JSON Response using a header we received
   return json(posts);
