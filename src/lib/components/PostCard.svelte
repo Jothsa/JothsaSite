8c17b2371c11d2649496deb38c9e3bd8fe7990c7
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-20 12:40:46 -0500

    unread badge

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -9,10 +9,23 @@
 </script>
 
 <li class="post">
-  <a href={`/blog/${post.slug}`} class="title styled-link">{post.title}</a>
+  <a class="unread" href={`/blog/${post.slug}`} aria-hidden="true" inert
+    ><!-- svelte-ignore a11y-missing-content --><a
+      class="unread-l"
+      href={`/blog/${post.slug}`}
+      aria-hidden="true"
+      inert />(unread!)<!-- svelte-ignore a11y-missing-content --><a
+      class="unread-r"
+      href={`/blog/${post.slug}`}
+      aria-hidden="true"
+      inert /></a>
+  <div class="post-border" />
+  <span class="post-link">
+    <a href={`/blog/${post.slug}`} class="title styled-link">{post.title}</a>
+  </span>
   <span class="date">{formatDate(post.date)}</span>
   <div class="categories">
-    <Categories categories={post.categories} />
+    <Categories categories={post.categories} catClasses="compact" />
   </div>
   <p class="description">{post.description}</p>
 </li>
@@ -24,7 +37,7 @@
     syntax: '<angle>';
   }
 
-  @keyframes post-card-grad-anim {
+  @keyframes border-grad-anim {
     from {
       --border-grad-angle: 0deg;
     }
@@ -35,7 +48,7 @@
   }
 
   .post {
-    --card-bg: var(--primary-bg);
+    --card-bg: oklch(33.53% 0.183 282.8);
     --border-grad-angle: 0deg;
     --border-grad-angle-init: 223deg;
     --_border-grad-angle: calc(
@@ -53,15 +66,18 @@
       var(--border-grad-color-2) 100%
     );
 
-    display: grid;
-    max-inline-size: 30ch;
+    position: relative;
+
+    /* display: grid; */
     padding: var(--space-2xs-cqi);
     border: var(--border);
-    background: var(--primary-bg);
+    background: var(--card-bg);
     border-image: var(--border-grad) 1;
     contain: content;
     container: post-card / inline-size;
     gap: var(--space-3xs);
+
+    /* grid-template-columns: repeat(2, fit-content(15ch)); */
     transition: transform 300ms ease-in-out;
 
     &:nth-of-type(odd) {
@@ -82,11 +98,61 @@
       text-wrap: balance;
     }
 
+    & .unread {
+      --ribbon-bg: var(--contrast);
+      position: absolute;
+      top: 0;
+      right: 0;
+
+      padding: 0;
+      padding-block: 0.5ch;
+      margin: 0;
+      background-color: var(--ribbon-bg);
+      font-size: var(--step--1-cqi);
+      font-weight: 600;
+      text-align: center;
+      text-decoration: none;
+      transform: translateX(30%) translateY(0%) rotate(45deg);
+      transform-origin: top left;
+      user-select: none;
+
+      & .unread-l,
+      & .unread-r {
+        position: absolute;
+        top: 0;
+
+        width: 100%;
+        height: 100%;
+        margin: 0 -1px; /* tweak */
+        background-color: inherit;
+        content: '';
+      }
+
+      & .unread-l {
+        inset-inline-end: 100%;
+      }
+
+      & .unread-r {
+        inset-inline-start: 100%;
+      }
+
+      &:visited {
+        background-color: var(--card-bg);
+        color: var(--card-bg);
+      }
+
+      & :is(.unread-l, .unread-r):visited {
+        background-color: var(--card-bg);
+        color: var(--card-bg);
+      }
+    }
+
     & .categories {
       display: flex;
+      width: 100%;
       flex-wrap: wrap;
 
-      @container post-card (inline-size > 40ch) {
+      @container post-card (inline-size < 40ch) {
         display: none;
       }
     }
@@ -103,7 +169,7 @@
   /* whyyyyyyyyyyy */
   @media (prefers-reduced-motion: no-preference) {
     .post:hover {
-      animation: post-card-grad-anim 500ms 120ms ease-in-out 1;
+      animation: border-grad-anim 500ms 120ms var(--ease-in-out-2) 1;
     }
   }
 </style>
