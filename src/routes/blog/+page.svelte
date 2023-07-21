91f1f6e253307c5eb6087eec43a6847b16c7d0e9
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-20 20:01:22 -0500

    add categories

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
   import PostsDisplay from '$components/PostsDisplay.svelte';
+  import Categories from '$components/Categories.svelte';
   import type { PageData } from './$types';
   export let data: PageData;
 </script>
 
-<!-- Posts -->
-<PostsDisplay posts={data.posts} />
+<div class="grid">
+  <PostsDisplay posts={data.posts} />
+  <Categories />
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    gap: var(--space-l);
+    grid-template-columns: repeat(2, auto);
+  }
+</style>
