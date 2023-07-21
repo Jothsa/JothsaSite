1db899ba09b9e5be62e3dd993100fe11b4de60ea
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-07-21 09:13:38 -0500

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,53 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
 import path from 'path';
+import shiki from 'shiki';
+// import {
+//   createShikiHighlighter,
+//   runTwoSlash,
+//   renderCodeToHTML,
+// } from 'shiki-twoslash';
+import { readFileSync } from 'fs';
+import emoji from 'remark-emoji';
+import a11yEmoji from '@fec/remark-a11y-emoji';
+
+// From https://github.com/whizkydee/vscode-palenight-theme/blob/master/themes/palenight.json
+const darkModernTheme = JSON.parse(readFileSync('./darkModernTheme.json'));
 
 /**  @type {import('mdsvex').MdsvexOptions  } */
 const mdsvexOptions = {
   extensions: ['.md'],
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      const highlighter = await shiki.getHighlighter({ theme: darkModernTheme });
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang }));
+      return `{@html \`${html}\` }`;
+    },
+    // highlight: {
+    //   highlighter: async (code, lang, meta) => {
+    //     const highlighter = await createShikiHighlighter({
+    //       theme: palenightTheme,
+    //     });
+
+    //     let twoslashResults = null;
+    //     if (meta && meta.includes('twoslash')) {
+    //       twoslashResults = runTwoSlash(code, lang, {});
+    //     }
+
+    //     const html = renderCodeToHTML(
+    //       code,
+    //       lang,
+    //       meta || [],
+    //       {},
+    //       highlighter,
+    //       twoslashResults,
+    //     );
+
+    //     return `{@html \`${html}\` }`;
+    //   },
+  },
+  remarkPlugins: [[emoji, { accessible: true }], a11yEmoji],
 };
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +55,7 @@ const config = {
   extensions: ['.svelte', '.md'],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  preprocess: [mdsvex(mdsvexOptions), vitePreprocess()],
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
