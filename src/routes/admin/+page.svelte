ce3440ac8caa6912351fc14f04ff8a62b5715079
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-03 13:32:12 -0500

    admin wip

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+</script>
+
+<nav class="admin-nav styled-links" aria-label="admin navigation">
+  <ul>
+    <li><a href="/admin">Admin Home</a></li>
+    <li>
+      <span>Create</span>
+      <ul>
+        <li><a href="/admin/create/post">Post</a></li>
+        <li><a href="/admin/create/blurb">Blurb</a></li>
+      </ul>
+    </li>
+    <li><a href="/admin/settings">Settings</a></li>
+  </ul>
+</nav>
+
+<main>
+  <p>Welcome, Jothsa!</p>
+</main>
+
+<style>
+  .admin-nav {
+    & * {
+      outline: blue 1px solid;
+    }
+
+    & ul {
+      display: flex;
+      inline-size: fit-content;
+      flex-direction: row;
+      gap: var(--space-xs);
+      list-style: none;
+
+      & li:has(ul) {
+        position: relative;
+      }
+
+      & li ul {
+        position: absolute;
+        inset-block-start: 2ch;
+        inset-inline-end: 0;
+        display: flex;
+        inline-size: fit-content;
+        flex-direction: column;
+        padding: 0;
+        margin: 0;
+        gap: var(--space-2xs);
+      }
+    }
+  }
+</style>
