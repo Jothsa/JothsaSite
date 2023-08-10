5c24e48fff7f09b33e21e38bc77e10e69e6052a0
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-10 10:47:02 -0500

    transitions wip

--- src/lib/components/PostsDisplay.svelte
+++ src/lib/components/PostsDisplay.svelte
@@ -13,10 +13,39 @@
 </section>
 
 <style>
+  @keyframes animate-in-and-out {
+    entry 0% {
+      opacity: 0;
+      transform: translateY(100%);
+    }
+
+    entry 100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+    exit 0% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+    exit 100% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+  }
+
   .posts {
     display: grid;
     padding: 0;
     margin: 0;
     gap: var(--space-m);
+
   }
+
+
+    :global(.posts .post) {
+      animation: linear animate-in-and-out;
+      animation-timeline: view();
+    }
 </style>
