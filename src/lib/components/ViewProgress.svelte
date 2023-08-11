f0f1426d9b4ce55fbc56af5793dc2584c50597d2
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-11 18:24:26 -0500

    improvements

--- src/lib/components/ViewProgress.svelte
+++ src/lib/components/ViewProgress.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   export let viewTimelineName: string;
+  export let style = '';
 
   // rn should be child of el whose progress is being tracked
 
   // progress = 100% when top or bottom of element has crossed viewport
   export let endOn: 'top' | 'bottom' = 'bottom';
+
+  // NOTE webkit mask on post is cutting off progress
 </script>
 
 <div
   class={`view-progress end-on-${endOn}`}
   aria-hidden="true"
-  style={`--view-timeline-name: ${viewTimelineName}`}>
+  style={`--view-timeline-name: ${viewTimelineName}; ${style}`}>
 </div>
 
 <style>
@@ -26,18 +29,28 @@
 
   .view-progress {
     --height: clamp(0.75ch, 1svh, 15px);
+    --bg: linear-gradient(
+      to right,
+      color-mix(in oklch, var(--accent), var(--decrease-contrast) 30%),
+      25%,
+      var(--accent),
+      75%,
+      color-mix(in oklch, var(--accent), var(--increase-contrast) 50%)
+    );
 
-    position: fixed;
+    /* with position: sticky, takes up space in the document flow/layout/w/e */
+    position: sticky;
     z-index: 5;
     inset-block-start: 0;
     inset-inline: 0;
     display: none;
+    inline-size: 100%;
     block-size: var(--height);
-    border-radius: 99ch;
+    border-radius: 0ch;
     animation: view-progress linear both;
     animation-range: exit-crossing 0% entry-crossing 100%;
     animation-timeline: var(--view-timeline-name);
-    background-color: var(--accent);
+    background: var(--bg);
     contain: content;
     transform-origin: 0 50%;
 
@@ -48,9 +61,13 @@
     @supports (
       (animation-range: exit-crossing 0% entry-crossing 100%) and
         (animation-timeline: var(--view-timeline-name)) and
-        (view-timeline: --post block)
+        (view-timeline: --post block) and (timeline-scope: --post)
     ) {
       display: unset;
+    }
+
+    @media (prefers-reduced-motion) {
+      display: none;
     }
   }
 </style>
