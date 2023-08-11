044f87a179f56e958f0d9db4db08a0fddbda494f
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-11 18:24:06 -0500

    more tests, details, overall improvements

--- src/lib/components/SupportsNotice.svelte
+++ src/lib/components/SupportsNotice.svelte
@@ -23,7 +23,7 @@
     unsupportedTestClassNames.push(`.no-${t}`);
   });
   // have to add my custom tests
-  tests.push('.no-popover', '.no-animation-range');
+  tests.push('.no-popover', '.no-animation-range', '.no-timeline-scope');
 
   onMount(() => {
     // doesn't need to be added to test array
@@ -36,6 +36,10 @@
       'animation-range',
       CSS.supports('animation-range: exit-crossing 0% entry-crossing 100%'),
     );
+    SupportsCSS.addTest(
+      'timeline-scope',
+      CSS.supports('timeline-scope: --post'),
+    );
     SupportsCSS.init({ tests });
     document
       .querySelector(
@@ -45,75 +49,98 @@
   });
 </script>
 
-<details id="supports-menu" class="styled-links">
-  <summary
-    >Your browser doesn't support the latest features used on this site.</summary>
-  <p>
-    We live on the edge here. To get the best experience, upgrade your browser.
-  </p>
+<div class="support-menus-container">
+  <noscript>
+    <div id="no-js-menu">
+      <p>Enable javascript for the best experience.</p>
+    </div>
+  </noscript>
 
-  <figure>
-    <figcaption class="">Unsupported features include:</figcaption>
-    <ul>
-      <li id="popover">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/API/Popover_API"
-          >The Popover API</a>
-      </li>
-      <li id="anchor-pos">
-        <a
-          href="https://developer.chrome.com/blog/tether-elements-to-each-other-with-css-anchor-positioning/"
-          >Anchor Positioning</a>
-      </li>
-      <li id="view-timeline">
-        <a
-          href="https://developer.chrome.com/articles/scroll-driven-animations/#view-progress-timeline"
-          >View Timeline</a>
-      </li>
-      <li id="Animation Range">
-        <a
-          href="https://developer.mozilla.org/en-US/docs/Web/CSS/animation-range"
-          >Animation-Range</a>
-      </li>
-      <li id="container">
-        <a
-          href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_container_queries"
-          >Container Queries</a>
-      </li>
-      <li id="at-property">
-        <a href="https://web.dev/at-property/">@property</a>
-      </li>
-      <li id="nesting">
-        <a href="https://developer.chrome.com/articles/css-nesting/"
-          >CSS Nesting</a>
-      </li>
-      <li id="has">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:has">:has</a>
-      </li>
-      <li id="dynamic-viewport-units">
-        <a href="https://css-tricks.com/the-large-small-and-dynamic-viewports/"
-          >Dynamic Viewport Units</a>
-      </li>
-      <li id="view-transitions">
-        <a
-          href="https://developer.chrome.com/docs/web-platform/view-transitions/"
-          >View Transitions</a>
-      </li>
-      <li id="logical-properties">
-        <a
-          href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_logical_properties_and_values"
-          >Logical Properties</a>
-      </li>
-    </ul>
-  </figure>
-</details>
+  <details id="supports-menu" class="styled-links">
+    <summary
+      >Your browser doesn't support the latest features used on this site.</summary>
+    <p>
+      We live on the edge here. To get the best experience, upgrade your
+      browser.
+    </p>
 
-<noscript>
-  <div id="no-js-menu">
-    <p>Enable javascript for the best experience.</p>
-  </div>
-</noscript>
+    <figure>
+      <figcaption class="">Unsupported features include:</figcaption>
+      <ul>
+        <noscript
+          ><li id="no-script-li">
+            Whoops, can't display without javascript.
+          </li></noscript>
+        <li id="popover">
+          <a href="https://developer.mozilla.org/en-US/docs/Web/API/Popover_API"
+            >The Popover API</a>
+        </li>
+        <li id="anchor-pos">
+          <a
+            href="https://developer.chrome.com/blog/tether-elements-to-each-other-with-css-anchor-positioning/"
+            >Anchor Positioning</a>
+        </li>
+        <li id="view-timeline">
+          <a
+            href="https://developer.chrome.com/articles/scroll-driven-animations/#view-progress-timeline"
+            >View Timeline</a>
+        </li>
+        <li id="Animation Range">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS/animation-range"
+            >Animation-Range</a>
+        </li>
+        <li id="container">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_container_queries"
+            >Container Queries</a>
+        </li>
+        <li id="at-property">
+          <a href="https://web.dev/at-property/">@property</a>
+        </li>
+        <li id="nesting">
+          <a href="https://developer.chrome.com/articles/css-nesting/"
+            >CSS Nesting</a>
+        </li>
+        <li id="has">
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:has"
+            >:has</a>
+        </li>
+        <li id="dynamic-viewport-units">
+          <a
+            href="https://css-tricks.com/the-large-small-and-dynamic-viewports/"
+            >Dynamic Viewport Units</a>
+        </li>
+        <li id="view-transitions">
+          <a
+            href="https://developer.chrome.com/docs/web-platform/view-transitions/"
+            >View Transitions</a>
+        </li>
+        <li id="timeline-scope">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS/timeline-scope"
+            ><code>timeline-scope</code></a>
+        </li>
+        <li id="logical-properties">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_logical_properties_and_values"
+            >Logical Properties</a>
+        </li>
+      </ul>
+    </figure>
+  </details>
+</div>
 
 <style>
+
+/* tbh I might be using too many modern features for a fallback menu but w/e */
+
+  .support-menus-container {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-2xs);
+  }
+
   /* some cls in inevitable, but try to minimize w/@supports 
    * starting dialog closed helps as well */
   :where(#supports-menu) {
@@ -159,10 +186,27 @@
   /* should prob only display ul if an li is visible 
    * (some unsupported features could not be listed, but I could just make sure to add all tested features to the list)
    */
-  :where(#supports-menu ul li) {
-    display: none;
+  :where(#supports-menu ul, #supports-menu li) {
     padding: 0;
     margin: 0;
+  }
+
+  @counter-style unsupported-list {
+  system: cyclic;
+  symbols: 🤖 👾 💾;
+  suffix: " ";
+}
+
+  :where(#supports-menu ul) {
+    list-style: unsupported-list;
+  }
+
+  #supports-menu li {
+    margin-inline-start: 2ch;
+  }
+
+  :where(#supports-menu ul li:not(#no-script-li)) {
+    display: none;
   }
 
   :global(
@@ -178,6 +222,6 @@
       :root.no-view-transitions #view-transitions,
       :root.no-logical-properties #logical-properties
     ) {
-    display: block;
+    display: list-item;
   }
 </style>
