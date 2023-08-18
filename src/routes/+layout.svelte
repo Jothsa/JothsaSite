f610e2dd2b1a8647941a151d63d82acbcfd7dcb7
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-18 15:02:01 -0500

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import '$styles/index.css';
   import AccessibleMenu from '$components/AccessibilityMenu.svelte';
   import Header from '$components/Header.svelte';
+  import Preferences from '$components/Preferences.svelte';
   import { page } from '$app/stores';
   import Footer from '$components/Footer.svelte';
   import { description, siteTitle } from '$utils/config';
