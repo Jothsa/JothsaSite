2568506b4de454c26e61cf455e9c88a3ae0e6f6e
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-21 14:13:02 -0500

    custom base 64 import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from '@zerodevx/svelte-img/vite';
+import type { Plugin } from 'vite';
 // import { sentrySvelteKit } from '@sentry/sveltekit';
 import { defineConfig } from 'vitest/config';
 // import { postBuildCommands } from './src/lib/utils/postBuild';
+import { readFileSync } from 'fs';
+
+const hexLoader: Plugin = {
+  name: 'b64-loader',
+  transform(code, id) {
+    const [path, query] = id.split('?');
+    if (query != 'b64') return null;
+
+    const data = readFileSync(path);
+    const hex = data.toString('base64url');
+
+    return `export default '${hex}';`;
+  },
+};
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     imagetools(),
+    hexLoader,
     // {
     //   name: 'postbuild-commands', // the name of your custom plugin. Could be anything.
     //   closeBundle: async () => {
