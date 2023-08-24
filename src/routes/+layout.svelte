758d1da0bae5ae6fc9bf60722b1be4264c98b67a
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-08-24 11:35:31 -0500

    random props wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,30 @@
   import { page } from '$app/stores';
   import Footer from '$components/Footer.svelte';
   import { description, siteTitle } from '$utils/config';
-
+  import { onMount } from 'svelte';
+  import { setRandomProps } from '$scripts/randomStyleProps';
   $: pageTitle =
     $page.data?.title !== undefined
       ? `${siteTitle} - ${$page.data.title}`
       : siteTitle;
   $: pageDescription = $page.data?.description || description;
+
+  onMount(() => {
+    setRandomProps([''], 2, 0, 1, 2);
+  });
 </script>
 
 <svelte:head>
   <link rel="alternate" type="application/atom+xml" href="/rss.xml" />
+  <!-- crossorigin is needed when preloading font files
+  @link https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/preload#cors-enabled_fetches:~:text=As%20mentioned%20above%2C%20one%20interesting%20case%20where%20this%20applies%20is%20font%20files.%20Because%20of%20various%20reasons%2C%20these%20have%20to%20be%20fetched%20using%20anonymous%2Dmode%20CORS%20(see%20Font%20fetching%20requirements).
+-->
+  <link
+    rel="preload"
+    href="/fonts/CarterOne.woff2"
+    as="font"
+    type="font/woff2"
+    crossorigin="anonymous" />
   <meta name="description" content={pageDescription} />
   <meta name="color-scheme" content="dark light" />
   <meta name="theme-color" content="oklch(19.41% 0.123 275.29)" />
