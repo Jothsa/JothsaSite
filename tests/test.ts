63ed2318156d580bd2a73d068ccf7bdcc2860bf9
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-09-02 11:42:51 -0500

    tests wip

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,26 @@
-import { expect, test } from '@playwright/test';
+import { expect, test, type Page } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-  await page.goto('/');
-  await expect(
-    page.getByRole('heading', { name: 'Welcome to SvelteKit' }),
-  ).toBeVisible();
+// make sure each page has an element with an id of "content" and "footer" for the quick nav
+test('every page has a #content element', async ({ page }) => {
+  // should be ok to only tests the main pages
+  const pageNames = [
+    '/',
+    '/blog',
+    '/blog/ahsoka-eps-1-2',
+    '/about',
+    '/tags',
+    '/tags/star-wars',
+  ];
+  pageNames.forEach(async (p) => {
+    console.log(`testing ${p}`);
+    await page.goto(p);
+    await testPageNavLandmarks(page);
+  });
 });
+
+async function testPageNavLandmarks(page: Page) {
+  const landmarks = ['header', 'content', 'footer'];
+  landmarks.forEach(async (l) => {
+    await expect(page.locator(`#${l}`)).toBeVisible();
+  });
+}
