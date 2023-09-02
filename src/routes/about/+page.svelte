ee803382db9d467aea184e2d83f342bead4181b2
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-09-02 11:45:04 -0500

    wip

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Tweet from '$components/Tweet.svelte';
+
   export let data;
   const feed = data.feed;
   console.log(feed);
@@ -15,4 +17,21 @@
 
 <main class="styled-links" id="content">
   <p>All about meeeee</p>
+
+  <div class="tweets">
+    {#each feed.items as item}
+      {#if item.title && item.pubDate && item.creator && item.link}
+        <Tweet tweetTitle={item.title} date={item.pubDate} author={item.creator} link={item.link}
+          >{@html item.content}</Tweet>
+      {/if}
+    {/each}
+  </div>
 </main>
+
+<style>
+  .tweets {
+    display: flex;
+    flex-flow: column;
+    gap: var(--space-xs);
+  }
+</style>
