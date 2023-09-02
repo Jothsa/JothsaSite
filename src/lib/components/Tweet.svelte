26c6d0ada35bfbc71e6a0e4a5cffa3551d27e4a1
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-09-02 11:43:07 -0500

    wip

---
+++ src/lib/components/Tweet.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let account: string = '@GabriellaSaab_';
+  export let author: string = '@GabriellaSaab_';
+  const retweet = author !== account;
+  export let tweetTitle: string;
+  export let link: string;
+  export let date: string;
+
+  // 2 kinds of retweets
+  // if a comment is added to the retweet, the tweet is embedded and must be fetched
+  // if no comment, only the title is changed
+</script>
+
+<div class="tweet">
+  <a href={link}>{tweetTitle}</a>
+  <p>{date}</p>
+  <slot></slot>
+</div>
+
+<style>
+  div {
+    inline-size: 30ch;
+    padding: var(--space-2xs);
+    border: solid 2px var(--accent);
+  }
+</style>
