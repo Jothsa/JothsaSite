9f782f0949aae85a7f10d760c3525d8f801a3938
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-09-04 12:14:42 -0500

    wip

---
+++ src/lib/components/Event.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  // import { date } from "valibot";
+  // import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
+  import type { Temporal } from '@js-temporal/polyfill';
+
+  type Event = {
+    name: string;
+    description: string;
+    date: Temporal.ZonedDateTime;
+    time: string;
+    location: string;
+    image: string;
+    link: string;
+  };
+  export let event: Event;
+</script>
+
+<div class="event-container">
+  <div class="calender-date">
+    <span class="day-number">{event.date.day}</span><span class="month"
+      >{event.date.month.toLocaleString(undefined, {
+        compactDisplay: 'long',
+      })}</span>
+  </div>
+</div>
+
+<style>
+  .event-container {
+    container: event / inline-size;
+  }
+
+  .calendar-date {
+    display: flex;
+    flex-flow: column nowrap;
+
+    & .day-number {
+      flex-grow: 2;
+      font-size: 5ch;
+      font-weight: 700;
+    }
+
+
+/* 
+    & .month {
+
+    } */
+  }
+</style>
