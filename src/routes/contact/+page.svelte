96488509eb30fafc0d5b555b576420389d57acf6
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-09-30 20:20:46 -0500

    error messages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,12 +7,13 @@
     <h2>Contact</h2>
     <div><span>All fields required</span></div>
     <label
-      >Name<input
-        type="text"
-        required
-        autocomplete="name"
-        name="name" /></label>
-    <label for="email">Email<input type="email" name="email" required /></label>
+      >Name<input type="text" required autocomplete="name" name="name" /><span
+        class="error-message">Name is required</span
+      ></label>
+    <label for="email"
+      >Email<input type="email" name="email" required /><span
+        class="error-message">A valid email is required</span
+      ></label>
     <label
       >Message<textarea
         name="message"
@@ -21,7 +22,7 @@
         placeholder="Hey Gabriella,
 I was reading your book and..."
         rows="5"></textarea
-      ></label>
+      ><span class="error-message">Message is required</span></label>
     <label>Are you not a non-robot?<input type="checkbox" /></label>
     <button type="submit" class="button-contrast target-aaa">Submit</button>
   </form>
@@ -35,6 +36,7 @@ I was reading your book and..."
 
   form {
     display: grid;
+    inline-size: 100%;
     min-inline-size: min(80svi, 60ch);
     gap: var(--space-s);
     grid-auto-flow: row;
@@ -47,15 +49,26 @@ I was reading your book and..."
 
   label {
     display: flex;
+    inline-size: max-content;
     flex-flow: row wrap;
     align-items: center;
     font-size: var(--step-1);
     gap: var(--space-xs);
+
+    &:has(textarea) {
+      flex-flow: column wrap;
+      align-items: start;
+    }
   }
+
+  /*
+   * [1] - needed because of the label flexbox align-items: start to place text at start
+   */
 
   textarea {
     min-inline-size: min(80svi, 60ch);
     min-block-size: min(20svi, 10ch);
+    align-self: center; /* [1] */
     padding: var(--space-3xs);
     font-size: var(--step-0);
   }
@@ -74,20 +87,33 @@ I was reading your book and..."
     font-size: var(--step-1);
   }
 
-  input:is([type='text'], [type='search'], [type='email']),
-  textarea {
+  .error-message {
+    display: none;
+    font-size: var(--step-0);
+  }
+
+  textarea,
+  input:is([type='text'], [type='search'], [type='email']) {
     --error-bg: color-mix(in oklch, var(--error), var(--decrease-contrast) 45%);
 
     @supports not selector(input:user-invalid) {
       &:required:invalid:not(:focus, :placeholder-shown) {
         border-color: var(--error);
         background-color: var(--error-bg);
+
+        & + .error-message {
+          display: unset;
+        }
       }
     }
 
     &:required:user-invalid:not(:focus, :placeholder-shown) {
       border-color: var(--error);
       background-color: var(--error-bg);
+
+      & + .error-message {
+        display: unset;
+      }
     }
   }
 </style>
